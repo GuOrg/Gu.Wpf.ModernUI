95eae750563a47e1a7b1397376c61751c5aed4e2
Johan Larsson <johan_gavle1@hotmail.com>
2015-01-08 19:43:53 +0100

    Restructured folders.

---
+++ FirstFloor.ModernUI.App/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+
+namespace FirstFloor.ModernUI.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ FirstFloor.ModernUI/Windows/Navigation/LinkCommands.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace FirstFloor.ModernUI.Windows.Navigation
+{
+    /// <summary>
+    /// The routed link commands.
+    /// </summary>
+    public static class LinkCommands
+    {
+        private static readonly RoutedUICommand navigateLink = new RoutedUICommand(Resources.NavigateLink, "NavigateLink", typeof(LinkCommands));
+
+        /// <summary>
+        /// Gets the navigate link routed command.
+        /// </summary>
+        public static RoutedUICommand NavigateLink
+        {
+            get { return navigateLink; }
+        }
+    }
+}
